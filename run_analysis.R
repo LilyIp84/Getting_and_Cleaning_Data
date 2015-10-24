27d6484f780bbb66097ca55df7e06b9f03809047
PumpkinPi <lily.ip.84@gmail.com>
2015-10-24 21:08:48 +0100

    Create run_analysis.R

---
+++ run_analysis.R
@@ -0,0 +1,22 @@
+setwd("./Project")
+
+options(stringsAsFactors = FALSE)
+options(scipen=10, digits = 2)
+
+## Merge training and test data sets into one data set
+trainData <- read.table("./train/X_train.txt")
+trainLabel <- read.table("./train/y_train.txt")
+trainSubject <- read.table("./train/subject_train.txt")
+testData <- read.table("./test/X_test.txt")
+testLabel <- read.table("./test/y_test.txt")
+testSubject <- read.table("./test/subject_test.txt")
+features <- read.table("features.txt")
+features <- features[[2]]
+activityLabel <- read.table("activity_labels.txt")
+
+allObservations <- rbind(trainData, testData)
+
+##Add features as column anmes for allObservations
+colnames(allObservations) <- features
+
+
